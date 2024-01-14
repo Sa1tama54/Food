9b31dfd30c398b5c8e94f402ee704bf7bfce5335
Sulaiman <ibn.alauddin123@gmail.com>
2024-01-14 23:04:38 +0300

    добавлен класс MenuCard для создания и отрисовки карточек меню

--- dist/js/script.js
+++ dist/js/script.js
@@ -100,7 +100,9 @@ document.addEventListener("DOMContentLoaded", () => {
   modalTrigger.forEach(btn => {
     btn.addEventListener("click", openModal); // Открытие модального окна при клике на кнопку
   });
-  const modalId = setTimeout(openModal, 3000);
+
+  // const modalId = setTimeout(openModal, 3000);
+
   const closeModal = () => {
     // Закрытие модального окна
     modal.classList.add("hide");
@@ -123,13 +125,48 @@ document.addEventListener("DOMContentLoaded", () => {
       closeModal();
     }
   });
-  const openModalAfterScroll = () => {
-    if (window.scrollY + document.documentElement.clientWidth >= document.documentElement.scrollHeight - 1) {
-      openModal();
-      window.removeEventListener("scroll", openModalAfterScroll);
+
+  // const openModalAfterScroll = () => {
+  //   if (
+  //     window.scrollY + document.documentElement.clientWidth >=
+  //     document.documentElement.scrollHeight - 1
+  //   ) {
+  //     openModal();
+  //     window.removeEventListener("scroll", openModalAfterScroll);
+  //   }
+  // };
+
+  // window.addEventListener("scroll", openModalAfterScroll);
+
+  // Menu Cards
+  class MenuCard {
+    constructor(src, alt, title, desc, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.desc = desc;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
     }
-  };
-  window.addEventListener("scroll", openModalAfterScroll);
+    render() {
+      const element = document.createElement("div");
+      element.innerHTML = `          
+        <div class="menu__item">
+          <img src="${this.src}" alt="${this.alt}" />
+          <h3 class="menu__item-subtitle">${this.title}</h3>
+          <div class="menu__item-descr">${this.desc}</div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+          </div>
+        </div>`;
+      this.parent.append(element);
+    }
+  }
+  new MenuCard("img/tabs/vegy.jpg", "vegy", "Меню “Фитнес”", "Меню “Фитнес” - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!", "229", ".menu .container").render();
+  new MenuCard("img/tabs/elite.jpg", "elite", "Меню “Премиум”", "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!", "550", ".menu .container").render();
+  new MenuCard("img/tabs/post.jpg", "post", "Меню “Постное”", "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.", "430", ".menu .container").render();
 });
 /******/ })()
 ;
